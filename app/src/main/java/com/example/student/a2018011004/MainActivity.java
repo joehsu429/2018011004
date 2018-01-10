1e5dfc2419bd70aee1d900318e58babbeb7d2374
joe <kkwhsu@gmail.com>
2018-01-10 16:08:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a2018011004/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.student.a2018011004;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                String str_url = "https://www.mobile01.com/rss/news.xml";
+                URL url = null;
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(inputStream);
+                    BufferedReader br = new BufferedReader(isr);
+                    StringBuilder sb=new StringBuilder();
+                    String str;
+                    while ((str=br.readLine()) !=null)
+                    {
+                        sb.append(str);
+                    }
+                    String str1 = sb.toString();
+                    Log.d("NET", str1);
+                    final MyHandler dataHandler = new MyHandler();
+                    SAXParserFactory spf=SAXParserFactory.newInstance();
+                    SAXParser sp=spf.newSAXParser();
+                    XMLReader xr=sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(str1)));
+                    br.close();
+                    isr.close();
+                    inputStream.close();
+
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+}
+
