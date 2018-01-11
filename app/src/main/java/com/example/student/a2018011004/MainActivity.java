91cbbb5b21edd7b392fcdcb33fa4b82cb49125ec
joe <kkwhsu@gmail.com>
2018-01-11 16:20:42 +0800

    20180111內容

--- app/src/main/java/com/example/student/a2018011004/MainActivity.java
+++ app/src/main/java/com/example/student/a2018011004/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.student.a2018011004;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -25,59 +31,92 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
+    MyAdapter adapter;
+    MyHandler dataHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.listView);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it=new Intent(MainActivity.this,DetailActivity.class);
+                it.putExtra("link",dataHandler.newsItems.get(i).link);
+                startActivity(it);
+            }
+        });
     }
 
-    public void click1(View v) {
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                String str_url = "https://www.mobile01.com/rss/news.xml";
-                URL url = null;
-                try {
-                    url = new URL(str_url);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.connect();
-                    InputStream inputStream = conn.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(inputStream);
-                    BufferedReader br = new BufferedReader(isr);
-                    StringBuilder sb=new StringBuilder();
-                    String str;
-                    while ((str=br.readLine()) !=null)
-                    {
-                        sb.append(str);
-                    }
-                    String str1 = sb.toString();
-                    Log.d("NET", str1);
-                    final MyHandler dataHandler = new MyHandler();
-                    SAXParserFactory spf=SAXParserFactory.newInstance();
-                    SAXParser sp=spf.newSAXParser();
-                    XMLReader xr=sp.getXMLReader();
-                    xr.setContentHandler(dataHandler);
-                    xr.parse(new InputSource(new StringReader(str1)));
-                    br.close();
-                    isr.close();
-                    inputStream.close();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.menu_reload:
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        String str_url = "https://www.mobile01.com/rss/news.xml";
+                        URL url = null;
+                        try {
+                            url = new URL(str_url);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+                            InputStream inputStream = conn.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(inputStream);
+                            BufferedReader br = new BufferedReader(isr);
+                            StringBuilder sb=new StringBuilder();
+                            String str;
+                            while ((str=br.readLine()) !=null)
+                            {
+                                sb.append(str);
+                            }
+                            String str1 = sb.toString();
+                            Log.d("NET", str1);
+                            dataHandler = new MyHandler();
+                            SAXParserFactory spf=SAXParserFactory.newInstance();
+                            SAXParser sp=spf.newSAXParser();
+                            XMLReader xr=sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(str1)));
+                            br.close();
+                            isr.close();
+                            inputStream.close();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter = new MyAdapter(MainActivity.this,
+                                            dataHandler.newsItems);
+                                    lv.setAdapter(adapter);
+                                }
+                            });
+
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
--- app/src/main/java/com/example/student/a2018011004/MyHandler.java
+++ app/src/main/java/com/example/student/a2018011004/MyHandler.java
@@ -6,37 +6,108 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Student on 2018/1/10.
  */
 
 public class MyHandler extends DefaultHandler {
     boolean isTitle=false;
+    boolean isItem=false;
+    boolean isLink=false;
+    boolean isDescription=false;
+    StringBuilder linkSB= new StringBuilder();
+    StringBuilder descSB = new StringBuilder();
+    public ArrayList<Mobile01NewsItem> newsItems=new ArrayList<>();
+    Mobile01NewsItem item;
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
         //Log.d("NET",qName);
-        if(qName.equals("title"))
+        switch(qName)//java7之後才可以用switch 省去寫太多if
         {
-            isTitle=true;
+            case "title":
+                isTitle=true;
+                break;
+            case "item":
+                isItem=true;
+                item=new Mobile01NewsItem();
+                break;
+            case "link":
+                isLink=true;
+                break;
+            case "description":
+                isDescription=true;
+                descSB = new StringBuilder();
+                break;
         }
+
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
-        if (qName.equals("title"))
+        switch(qName)//java7之後才可以用switch 省去寫太多if
         {
-            isTitle = false;
+            case "title":
+                isTitle=false;
+                break;
+            case "item":
+                isItem=false;
+                Log.d("NET", "When add item, imgurl:" + item.imgurl);
+                newsItems.add(item);
+                break;
+            case "link":
+                isLink=false;
+                if(isItem)
+                {
+                    item.link=linkSB.toString();
+                    linkSB = new StringBuilder();
+                }
+                break;
+            case "description":
+                isDescription=false;
+                if (isItem)
+                {
+                    String str = descSB.toString();
+                    Log.d("NET", "end Element str:" + str);
+                    Pattern pattern = Pattern.compile("https.*jpg");
+                    Matcher m = pattern.matcher(str);
+                    String imgurl = "";
+                    if (m.find())
+                    {
+                        imgurl = m.group(0);
+                    }
+                    str = str.replaceAll("<img.*/>", "");
+                    item.description = str;
+                    item.imgurl = imgurl;
+                    Log.d("NET", "In Handler: Item.desc:" + item.description);
+                    Log.d("NET", "In Handler: Item.imgurl:" + item.imgurl);
+                }
+                break;
         }
+
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         super.characters(ch, start, length);
-        if (isTitle)
+        if (isTitle && isItem)
         {
             Log.d("NET",new String(ch,start,length));
+            item.title=new String(ch,start,length);
+        }
+        if (isLink && isItem)
+        {
+            Log.d("NET",new String(ch,start,length));
+            linkSB.append(new String(ch,start,length));
+        }
+        if (isDescription && isItem)
+        {
+            descSB.append(new String(ch, start, length));
         }
     }
 }
